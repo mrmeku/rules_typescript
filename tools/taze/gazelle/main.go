17f17a284dc4999b31602f0148b600801a7d9d24
mrmeku <mrmeku@gmail.com>
2017-11-22 14:25:12 -0700

    Initial commit of gazelle which will serve as the basis of taze.
    We are opting to use gazelle as started code for creating taze. Once
    we are done we plan to extract the common parts of both code basis
    so that they can be shared (and used to create BUILD file creation
    tools for other languages)

---
+++ tools/taze/gazelle/main.go
@@ -0,0 +1,476 @@
+/* Copyright 2016 The Bazel Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Command gazelle is a BUILD file generator for Go projects.
+// See "gazelle --help" for more details.
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"go/build"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	bf "github.com/bazelbuild/buildtools/build"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/config"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/merger"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/packages"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/resolve"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/rules"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/wspace"
+)
+
+type emitFunc func(*config.Config, *bf.File) error
+
+var modeFromName = map[string]emitFunc{
+	"print": printFile,
+	"fix":   fixFile,
+	"diff":  diffFile,
+}
+
+type command int
+
+const (
+	updateCmd command = iota
+	fixCmd
+)
+
+var commandFromName = map[string]command{
+	"update": updateCmd,
+	"fix":    fixCmd,
+}
+
+// visitRecord stores information about about a directory visited with
+// packages.Walk.
+type visitRecord struct {
+	// pkgRel is the slash-separated path to the visited directory, relative to
+	// the repository root. "" for the repository root itself.
+	pkgRel string
+
+	// buildRel is the slash-separated path to the directory containing the
+	// relevant build file for the directory being visited, relative to the
+	// repository root. "" for the repository root itself. This may differ
+	// from pkgRel in flat mode.
+	buildRel string
+
+	// rules is a list of generated Go rules.
+	rules []bf.Expr
+
+	// empty is a list of empty Go rules that may be deleted.
+	empty []bf.Expr
+
+	// oldFile is an existing build file in the directory. May be nil.
+	oldFile *bf.File
+}
+
+type byPkgRel []visitRecord
+
+func (vs byPkgRel) Len() int           { return len(vs) }
+func (vs byPkgRel) Less(i, j int) bool { return vs[i].pkgRel < vs[j].pkgRel }
+func (vs byPkgRel) Swap(i, j int)      { vs[i], vs[j] = vs[j], vs[i] }
+
+func run(c *config.Config, cmd command, emit emitFunc) {
+	shouldFix := c.ShouldFix
+	l := resolve.NewLabeler(c)
+
+	var visits []visitRecord
+
+	// Visit directories to modify.
+	// TODO: visit all directories in the repository in order to index rules.
+	for _, dir := range c.Dirs {
+		packages.Walk(c, dir, func(rel string, c *config.Config, pkg *packages.Package, oldFile *bf.File, isUpdateDir bool) {
+			// Fix existing files.
+			if oldFile != nil {
+				if shouldFix {
+					oldFile = merger.FixFile(c, oldFile)
+				} else {
+					fixedFile := merger.FixFile(c, oldFile)
+					if fixedFile != oldFile {
+						log.Printf("%s: warning: file contains rules whose structure is out of date. Consider running 'gazelle fix'.", oldFile.Path)
+					}
+				}
+			}
+
+			// TODO: Index rules in existing files.
+			// TODO: delete rules in directories where pkg == nil (no buildable
+			// Go code).
+
+			// Generate rules.
+			if pkg != nil {
+				var buildRel string
+				if c.StructureMode == config.FlatMode {
+					buildRel = ""
+				} else {
+					buildRel = rel
+				}
+				g := rules.NewGenerator(c, l, buildRel, oldFile)
+				rules, empty := g.GenerateRules(pkg)
+				visits = append(visits, visitRecord{
+					pkgRel:   rel,
+					buildRel: buildRel,
+					rules:    rules,
+					empty:    empty,
+					oldFile:  oldFile,
+				})
+			}
+		})
+
+		// TODO: resolve dependencies using the index.
+		resolver := resolve.NewResolver(c, l)
+		for _, v := range visits {
+			for _, r := range v.rules {
+				resolver.ResolveRule(r, v.pkgRel, v.buildRel)
+			}
+		}
+
+		// Merge old files and generated files. Emit merged files.
+		switch c.StructureMode {
+		case config.HierarchicalMode:
+			for _, v := range visits {
+				genFile := &bf.File{
+					Path: filepath.Join(c.RepoRoot, filepath.FromSlash(v.pkgRel), c.DefaultBuildFileName()),
+					Stmt: v.rules,
+				}
+				mergeAndEmit(c, genFile, v.oldFile, v.empty, emit)
+			}
+
+		case config.FlatMode:
+			sort.Stable(byPkgRel(visits))
+			var oldFile *bf.File
+			if len(visits) > 0 && visits[0].pkgRel == "" {
+				oldFile = visits[0].oldFile
+			}
+
+			genFile := &bf.File{Path: filepath.Join(c.RepoRoot, c.DefaultBuildFileName())}
+			var empty []bf.Expr
+			for _, v := range visits {
+				genFile.Stmt = append(genFile.Stmt, v.rules...)
+				empty = append(empty, v.empty...)
+			}
+			mergeAndEmit(c, genFile, oldFile, empty, emit)
+
+		default:
+			log.Panicf("unsupported structure mode: %v", c.StructureMode)
+		}
+	}
+}
+
+// mergeAndEmit merges "genFile" with "oldFile". "oldFile" may be nil if
+// no file exists. If v.c.ShouldFix is true, deprecated usage of old rules in
+// "oldFile" will be fixed. The resulting merged file will be emitted using
+// the "v.emit" function.
+func mergeAndEmit(c *config.Config, genFile, oldFile *bf.File, empty []bf.Expr, emit emitFunc) {
+	if oldFile == nil {
+		// No existing file, so no merge required.
+		rules.SortLabels(genFile)
+		genFile = merger.FixLoads(genFile)
+		bf.Rewrite(genFile, nil) // have buildifier 'format' our rules.
+		if err := emit(c, genFile); err != nil {
+			log.Print(err)
+		}
+		return
+	}
+
+	// Existing file. Fix it or see if it needs fixing before merging.
+	oldFile = merger.FixFileMinor(c, oldFile)
+	if c.ShouldFix {
+		oldFile = merger.FixFile(c, oldFile)
+	} else {
+		fixedFile := merger.FixFile(c, oldFile)
+		if fixedFile != oldFile {
+			log.Printf("%s: warning: file contains rules whose structure is out of date. Consider running 'gazelle fix'.", oldFile.Path)
+		}
+	}
+
+	// Existing file, so merge and replace the old one.
+	mergedFile := merger.MergeWithExisting(genFile, oldFile, empty)
+	if mergedFile == nil {
+		// Ignored file. Don't emit.
+		return
+	}
+
+	rules.SortLabels(mergedFile)
+	mergedFile = merger.FixLoads(mergedFile)
+	bf.Rewrite(mergedFile, nil) // have buildifier 'format' our rules.
+	if err := emit(c, mergedFile); err != nil {
+		log.Print(err)
+		return
+	}
+}
+
+func usage(fs *flag.FlagSet) {
+	fmt.Fprintln(os.Stderr, `usage: gazelle <command> [flags...] [package-dirs...]
+
+Gazelle is a BUILD file generator for Go projects. It can create new BUILD files
+for a project that follows "go build" conventions, and it can update BUILD files
+if they already exist. It can be invoked directly in a project workspace, or
+it can be run on an external dependency during the build as part of the
+go_repository rule.
+
+Gazelle may be run with one of the commands below. If no command is given,
+Gazelle defaults to "update".
+
+  update - Gazelle will create new BUILD files or update existing BUILD files
+      if needed.
+	fix - in addition to the changes made in update, Gazelle will make potentially
+	    breaking changes. For example, it may delete obsolete rules or rename
+      existing rules.
+
+Gazelle has several output modes which can be selected with the -mode flag. The
+output mode determines what Gazelle does with updated BUILD files.
+
+  fix (default) - write updated BUILD files back to disk.
+  print - print updated BUILD files to stdout.
+  diff - diff updated BUILD files against existing files in unified format.
+
+Gazelle accepts a list of paths to Go package directories to process (defaults
+to . if none given). It recursively traverses subdirectories. All directories
+must be under the directory specified by -repo_root; if -repo_root is not given,
+this is the directory containing the WORKSPACE file.
+
+Gazelle is under active delevopment, and its interface may change
+without notice.
+
+FLAGS:
+`)
+	fs.PrintDefaults()
+}
+
+func main() {
+	log.SetPrefix("gazelle: ")
+	log.SetFlags(0) // don't print timestamps
+
+	c, cmd, emit, err := newConfiguration(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	run(c, cmd, emit)
+}
+
+func newConfiguration(args []string) (*config.Config, command, emitFunc, error) {
+	cmd := updateCmd
+	if len(args) > 0 {
+		if c, ok := commandFromName[args[0]]; ok {
+			cmd = c
+			args = args[1:]
+		}
+	}
+
+	fs := flag.NewFlagSet("gazelle", flag.ContinueOnError)
+	// Flag will call this on any parse error. Don't print usage unless
+	// -h or -help were passed explicitly.
+	fs.Usage = func() {}
+
+	knownImports := multiFlag{}
+	buildFileName := fs.String("build_file_name", "BUILD.bazel,BUILD", "comma-separated list of valid build file names.\nThe first element of the list is the name of output build files to generate.")
+	buildTags := fs.String("build_tags", "", "comma-separated list of build tags. If not specified, Gazelle will not\n\tfilter sources with build constraints.")
+	external := fs.String("external", "external", "external: resolve external packages with go_repository\n\tvendored: resolve external packages as packages in vendor/")
+	var goPrefix explicitFlag
+	fs.Var(&goPrefix, "go_prefix", "prefix of import paths in the current workspace")
+	repoRoot := fs.String("repo_root", "", "path to a directory which corresponds to go_prefix, otherwise gazelle searches for it.")
+	fs.Var(&knownImports, "known_import", "import path for which external resolution is skipped (can specify multiple times)")
+	mode := fs.String("mode", "fix", "print: prints all of the updated BUILD files\n\tfix: rewrites all of the BUILD files in place\n\tdiff: computes the rewrite but then just does a diff")
+	flat := fs.Bool("experimental_flat", false, "whether gazelle should generate a single, combined BUILD file.\nThis mode is experimental and may not work yet.")
+	proto := fs.String("proto", "default", "default: generates new proto rules\n\tdisable: does not touch proto rules\n\tlegacy (deprecated): generates old proto rules")
+	experimentalPlatforms := fs.Bool("experimental_platforms", false, "generates separate select expressions for OS and arch-specific srcs and deps (won't work until Bazel 0.8)")
+	if err := fs.Parse(args); err != nil {
+		if err == flag.ErrHelp {
+			usage(fs)
+			os.Exit(0)
+		}
+		// flag already prints the error; don't print it again.
+		log.Fatal("Try -help for more information.")
+	}
+
+	var c config.Config
+	var err error
+
+	c.Dirs = fs.Args()
+	if len(c.Dirs) == 0 {
+		c.Dirs = []string{"."}
+	}
+	for i := range c.Dirs {
+		c.Dirs[i], err = filepath.Abs(c.Dirs[i])
+		if err != nil {
+			return nil, cmd, nil, err
+		}
+	}
+
+	if *repoRoot != "" {
+		c.RepoRoot = *repoRoot
+	} else if len(c.Dirs) == 1 {
+		c.RepoRoot, err = wspace.Find(c.Dirs[0])
+		if err != nil {
+			return nil, cmd, nil, fmt.Errorf("-repo_root not specified, and WORKSPACE cannot be found: %v", err)
+		}
+	} else {
+		cwd, err := filepath.Abs(".")
+		if err != nil {
+			return nil, cmd, nil, err
+		}
+		c.RepoRoot, err = wspace.Find(cwd)
+		if err != nil {
+			return nil, cmd, nil, fmt.Errorf("-repo_root not specified, and WORKSPACE cannot be found: %v", err)
+		}
+	}
+
+	for _, dir := range c.Dirs {
+		if !isDescendingDir(dir, c.RepoRoot) {
+			return nil, cmd, nil, fmt.Errorf("dir %q is not a subdirectory of repo root %q", dir, c.RepoRoot)
+		}
+	}
+
+	c.ValidBuildFileNames = strings.Split(*buildFileName, ",")
+	if len(c.ValidBuildFileNames) == 0 {
+		return nil, cmd, nil, fmt.Errorf("no valid build file names specified")
+	}
+
+	c.SetBuildTags(*buildTags)
+	c.PreprocessTags()
+
+	if goPrefix.set {
+		c.GoPrefix = goPrefix.value
+	} else {
+		c.GoPrefix, err = loadGoPrefix(&c)
+		if err != nil {
+			return nil, cmd, nil, fmt.Errorf("-go_prefix not set")
+		}
+		// TODO(jayconrod): read prefix directives when they are supported.
+	}
+	if strings.HasPrefix(c.GoPrefix, "/") || build.IsLocalImport(c.GoPrefix) {
+		return nil, cmd, nil, fmt.Errorf("invalid go_prefix: %q", c.GoPrefix)
+	}
+
+	c.ShouldFix = cmd == fixCmd
+
+	c.DepMode, err = config.DependencyModeFromString(*external)
+	if err != nil {
+		return nil, cmd, nil, err
+	}
+
+	if *flat {
+		c.StructureMode = config.FlatMode
+	} else {
+		c.StructureMode = config.HierarchicalMode
+	}
+
+	c.ProtoMode, err = config.ProtoModeFromString(*proto)
+	if err != nil {
+		return nil, cmd, nil, err
+	}
+
+	emit, ok := modeFromName[*mode]
+	if !ok {
+		return nil, cmd, nil, fmt.Errorf("unrecognized emit mode: %q", *mode)
+	}
+
+	c.KnownImports = append(c.KnownImports, knownImports...)
+
+	c.ExperimentalPlatforms = *experimentalPlatforms
+
+	return &c, cmd, emit, err
+}
+
+type explicitFlag struct {
+	set   bool
+	value string
+}
+
+func (f *explicitFlag) Set(value string) error {
+	f.set = true
+	f.value = value
+	return nil
+}
+
+func (f *explicitFlag) String() string {
+	if f == nil {
+		return ""
+	}
+	return f.value
+}
+
+func loadBuildFile(c *config.Config, dir string) (*bf.File, error) {
+	var buildPath string
+	for _, base := range c.ValidBuildFileNames {
+		p := filepath.Join(dir, base)
+		fi, err := os.Stat(p)
+		if err == nil {
+			if fi.Mode().IsRegular() {
+				buildPath = p
+				break
+			}
+			continue
+		}
+		if !os.IsNotExist(err) {
+			return nil, err
+		}
+	}
+	if buildPath == "" {
+		return nil, os.ErrNotExist
+	}
+
+	data, err := ioutil.ReadFile(buildPath)
+	if err != nil {
+		return nil, err
+	}
+	return bf.Parse(buildPath, data)
+}
+
+func loadGoPrefix(c *config.Config) (string, error) {
+	f, err := loadBuildFile(c, c.RepoRoot)
+	if err != nil {
+		return "", err
+	}
+	for _, s := range f.Stmt {
+		c, ok := s.(*bf.CallExpr)
+		if !ok {
+			continue
+		}
+		l, ok := c.X.(*bf.LiteralExpr)
+		if !ok {
+			continue
+		}
+		if l.Token != "go_prefix" {
+			continue
+		}
+		if len(c.List) != 1 {
+			return "", fmt.Errorf("found go_prefix(%v) with too many args", c.List)
+		}
+		v, ok := c.List[0].(*bf.StringExpr)
+		if !ok {
+			return "", fmt.Errorf("found go_prefix(%v) which is not a string", c.List)
+		}
+		return v.Value, nil
+	}
+	return "", errors.New("-go_prefix not set, and no go_prefix in root BUILD file")
+}
+
+func isDescendingDir(dir, root string) bool {
+	rel, err := filepath.Rel(root, dir)
+	if err != nil {
+		return false
+	}
+	if rel == "." {
+		return true
+	}
+	return !strings.HasPrefix(rel, "..")
+}
---
+++ tools/taze/resolve/resolve.go
@@ -0,0 +1,313 @@
+/* Copyright 2016 The Bazel Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resolve
+
+import (
+	"fmt"
+	"go/build"
+	"log"
+	"path"
+	"strings"
+
+	bf "github.com/bazelbuild/buildtools/build"
+	"github.com/bazelbuild/rules_go/go/tools/gazelle/config"
+)
+
+// Resolver resolves import strings in source files (import paths in Go,
+// import statements in protos) into Bazel labels.
+// TODO(#859): imports are currently resolved by guessing a label based
+// on the name. We should be smarter about this and build a table mapping
+// import paths to labels that we can use to cross-reference.
+type Resolver struct {
+	c        *config.Config
+	l        Labeler
+	external nonlocalResolver
+}
+
+// nonlocalResolver resolves import paths outside of the current repository's
+// prefix. Once we have smarter import path resolution, this shouldn't
+// be necessary, and we can remove this abstraction.
+type nonlocalResolver interface {
+	resolve(imp string) (Label, error)
+}
+
+func NewResolver(c *config.Config, l Labeler) *Resolver {
+	var e nonlocalResolver
+	switch c.DepMode {
+	case config.ExternalMode:
+		e = newExternalResolver(l, c.KnownImports)
+	case config.VendorMode:
+		e = newVendoredResolver(l)
+	}
+
+	return &Resolver{
+		c:        c,
+		l:        l,
+		external: e,
+	}
+}
+
+// ResolveRule modifies a generated rule e by replacing the import paths in the
+// "_gazelle_imports" attribute with labels in a "deps" attribute. This may
+// may safely called on expressions that aren't Go rules (nothing will happen).
+func (r *Resolver) ResolveRule(e bf.Expr, pkgRel, buildRel string) {
+	call, ok := e.(*bf.CallExpr)
+	if !ok {
+		return
+	}
+	rule := bf.Rule{Call: call}
+
+	var resolve func(imp, pkgRel string) (Label, error)
+	switch rule.Kind() {
+	case "go_library", "go_binary", "go_test":
+		resolve = r.resolveGo
+	case "proto_library":
+		resolve = r.resolveProto
+	case "go_proto_library", "go_grpc_library":
+		resolve = r.resolveGoProto
+	default:
+		return
+	}
+
+	imports := rule.AttrDefn(config.GazelleImportsKey)
+	if imports == nil {
+		return
+	}
+
+	deps := mapExprStrings(imports.Y, func(imp string) string {
+		label, err := resolve(imp, pkgRel)
+		if err != nil {
+			if _, ok := err.(standardImportError); !ok {
+				log.Print(err)
+			}
+			return ""
+		}
+		label.Relative = label.Repo == "" && label.Pkg == buildRel
+		return label.String()
+	})
+	if deps == nil {
+		rule.DelAttr(config.GazelleImportsKey)
+	} else {
+		imports.X.(*bf.LiteralExpr).Token = "deps"
+		imports.Y = deps
+	}
+}
+
+type standardImportError struct {
+	imp string
+}
+
+func (e standardImportError) Error() string {
+	return fmt.Sprintf("import path %q is in the standard library", e.imp)
+}
+
+// mapExprStrings applies a function f to the strings in e and returns a new
+// expression with the results. Scalar strings, lists, dicts, selects, and
+// concatenations are supported.
+func mapExprStrings(e bf.Expr, f func(string) string) bf.Expr {
+	switch expr := e.(type) {
+	case *bf.StringExpr:
+		s := f(expr.Value)
+		if s == "" {
+			return nil
+		}
+		return &bf.StringExpr{Value: s}
+
+	case *bf.ListExpr:
+		var list []bf.Expr
+		for _, elem := range expr.List {
+			elem = mapExprStrings(elem, f)
+			if elem != nil {
+				list = append(list, elem)
+			}
+		}
+		if len(list) == 0 && len(expr.List) > 0 {
+			return nil
+		}
+		return &bf.ListExpr{List: list}
+
+	case *bf.DictExpr:
+		var cases []bf.Expr
+		isEmpty := true
+		for _, kv := range expr.List {
+			keyval, ok := kv.(*bf.KeyValueExpr)
+			if !ok {
+				log.Panicf("unexpected expression in generated imports dict: %#v", kv)
+			}
+			value := mapExprStrings(keyval.Value, f)
+			if value != nil {
+				cases = append(cases, &bf.KeyValueExpr{Key: keyval.Key, Value: value})
+				if key, ok := keyval.Key.(*bf.StringExpr); !ok || key.Value != "//conditions:default" {
+					isEmpty = false
+				}
+			}
+		}
+		if isEmpty {
+			return nil
+		}
+		return &bf.DictExpr{List: cases}
+
+	case *bf.CallExpr:
+		if x, ok := expr.X.(*bf.LiteralExpr); !ok || x.Token != "select" || len(expr.List) != 1 {
+			log.Panicf("unexpected call expression in generated imports: %#v", e)
+		}
+		arg := mapExprStrings(expr.List[0], f)
+		if arg == nil {
+			return nil
+		}
+		call := *expr
+		call.List[0] = arg
+		return &call
+
+	case *bf.BinaryExpr:
+		x := mapExprStrings(expr.X, f)
+		y := mapExprStrings(expr.Y, f)
+		if x == nil {
+			return y
+		}
+		if y == nil {
+			return x
+		}
+		binop := *expr
+		binop.X = x
+		binop.Y = y
+		return &binop
+
+	default:
+		log.Panicf("unexpected expression in generated imports: %#v", e)
+		return nil
+	}
+}
+
+// resolveGo resolves an import path from a Go source file to a label.
+// pkgRel is the path to the Go package relative to the repository root; it
+// is used to resolve relative imports.
+func (r *Resolver) resolveGo(imp, pkgRel string) (Label, error) {
+	if build.IsLocalImport(imp) {
+		cleanRel := path.Clean(path.Join(pkgRel, imp))
+		if build.IsLocalImport(cleanRel) {
+			return Label{}, fmt.Errorf("relative import path %q from %q points outside of repository", imp, pkgRel)
+		}
+		imp = path.Join(r.c.GoPrefix, cleanRel)
+	}
+
+	switch {
+	case IsStandard(imp):
+		return Label{}, standardImportError{imp}
+	case imp == r.c.GoPrefix:
+		return r.l.LibraryLabel(""), nil
+	case r.c.GoPrefix == "" || strings.HasPrefix(imp, r.c.GoPrefix+"/"):
+		return r.l.LibraryLabel(strings.TrimPrefix(imp, r.c.GoPrefix+"/")), nil
+	default:
+		return r.external.resolve(imp)
+	}
+}
+
+const (
+	wellKnownPrefix     = "google/protobuf/"
+	wellKnownGoProtoPkg = "ptypes"
+	descriptorPkg       = "protoc-gen-go/descriptor"
+)
+
+// resolveProto resolves an import statement in a .proto file to a label
+// for a proto_library rule.
+func (r *Resolver) resolveProto(imp, pkgRel string) (Label, error) {
+	if !strings.HasSuffix(imp, ".proto") {
+		return Label{}, fmt.Errorf("can't import non-proto: %q", imp)
+	}
+	imp = imp[:len(imp)-len(".proto")]
+
+	if isWellKnown(imp) {
+		// Well Known Type
+		name := path.Base(imp) + "_proto"
+		return Label{Repo: config.WellKnownTypesProtoRepo, Name: name}, nil
+	}
+
+	rel := path.Dir(imp)
+	if rel == "." {
+		rel = ""
+	}
+	name := relBaseName(r.c, rel)
+	return r.l.ProtoLabel(rel, name), nil
+}
+
+// resolveGoProto resolves an import statement in a .proto file to a
+// label for a go_library rule that embeds the corresponding go_proto_library.
+func (r *Resolver) resolveGoProto(imp, pkgRel string) (Label, error) {
+	if !strings.HasSuffix(imp, ".proto") {
+		return Label{}, fmt.Errorf("can't import non-proto: %q", imp)
+	}
+	imp = imp[:len(imp)-len(".proto")]
+
+	if isWellKnown(imp) {
+		// Well Known Type
+		base := path.Base(imp)
+		if base == "descriptor" {
+			switch r.c.DepMode {
+			case config.ExternalMode:
+				label := r.l.LibraryLabel(descriptorPkg)
+				if r.c.GoPrefix != config.WellKnownTypesGoPrefix {
+					label.Repo = config.WellKnownTypesGoProtoRepo
+				}
+				return label, nil
+			case config.VendorMode:
+				pkg := path.Join("vendor", config.WellKnownTypesGoPrefix, descriptorPkg)
+				label := r.l.LibraryLabel(pkg)
+				return label, nil
+			default:
+				log.Panicf("unknown external mode: %v", r.c.DepMode)
+			}
+		}
+
+		switch r.c.DepMode {
+		case config.ExternalMode:
+			pkg := path.Join(wellKnownGoProtoPkg, base)
+			label := r.l.LibraryLabel(pkg)
+			if r.c.GoPrefix != config.WellKnownTypesGoPrefix {
+				label.Repo = config.WellKnownTypesGoProtoRepo
+			}
+			return label, nil
+		case config.VendorMode:
+			pkg := path.Join("vendor", config.WellKnownTypesGoPrefix, wellKnownGoProtoPkg, base)
+			return r.l.LibraryLabel(pkg), nil
+		default:
+			log.Panicf("unknown external mode: %v", r.c.DepMode)
+		}
+	}
+
+	// Temporary hack: guess the label based on the proto file name. We assume
+	// all proto files in a directory belong to the same package, and the
+	// package name matches the directory base name. We also assume that protos
+	// in the vendor directory must refer to something else in vendor.
+	// TODO(#859): use dependency table to resolve once it exists.
+	if pkgRel == "vendor" || strings.HasPrefix(pkgRel, "vendor/") {
+		imp = path.Join("vendor", imp)
+	}
+	rel := path.Dir(imp)
+	if rel == "." {
+		rel = ""
+	}
+	return r.l.LibraryLabel(rel), nil
+}
+
+// IsStandard returns whether a package is in the standard library.
+func IsStandard(imp string) bool {
+	return stdPackages[imp]
+}
+
+func isWellKnown(imp string) bool {
+	return strings.HasPrefix(imp, wellKnownPrefix) && strings.TrimPrefix(imp, wellKnownPrefix) == path.Base(imp)
+}
